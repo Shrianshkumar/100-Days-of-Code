f992f1fe808ee00f45a8f9905863755814e578bf
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-11-30 12:21:07 +0530

    Day3

---
+++ Day3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Day3{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter: ");
+        char ch=sc.next().charAt(0);
+        int num=ch;
+        System.out.println("ASCII Value = " +num);
+        
+    }}
