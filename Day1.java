9c511a3383278f2239d5e60661ff437994fa5c77
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-11-30 12:23:03 +0530

    Day1

--- Day1.java
+++ Day1.java
@@ -1,3 +1,30 @@
+Day 1 coding Statement: Write a program to identify if the character is a vowel or consonant.
+
+Description of the program: 
+
+
+Take an input character from the user and check whether the given input is a vowel or consonant.
+
+Input
+A
+
+Output
+Vowel
+
+Input
+m
+
+Output
+Consonant
+
+Input
+3
+
+Output
+Invalid Input
+
+
+
 import java.util.*;
 class Day1{
     public static void main(String[] args){
