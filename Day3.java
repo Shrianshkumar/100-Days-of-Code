73ac700dc3fd0ebdf821e2990855c216133d17db
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-11-30 12:24:57 +0530

    Day3

--- Day3.java
+++ Day3.java
@@ -1,3 +1,17 @@
+Day 3 coding Statement: Write a program to find ASCII values of a character
+
+ 
+
+Description: Get an input character from the user and give the ASCII value of the given input as the output.
+
+Input: b
+
+Output: 98
+
+Input: B
+
+Output: 66
+
 import java.util.*;
 class Day3{
     public static void main(String[] args){
