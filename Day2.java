8c29c17c9d4911aa4598c7224314465949391112
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-11-30 12:24:05 +0530

    Day2.java

--- Day2.java
+++ Day2.java
@@ -1,3 +1,20 @@
+Day 2 coding Statement: Write a program to identify if the character is an alphabet or not.
+
+ 
+
+Description: Take an input character from user and check whether it is an alphabet or not.
+
+ 
+
+Input : A
+
+Output:  Alphabet
+
+Input: 7
+
+Output: Not an alphabet
+
+
 import java.util.*;
 class Day2{
     public static void main(String[] args){
