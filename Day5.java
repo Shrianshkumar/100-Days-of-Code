d1928e30f7cfe59899b45317fdf5d85a7eac3bbd
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-05 19:38:38 +0530

    Day5

--- Day5.java
+++ Day5.java
@@ -1,3 +1,26 @@
+Day 5 coding Statement:  Write a program to identify if the number is even or odd
+
+Description
+
+Get a number as input from the user and check whether the given number is odd or even
+
+Input
+
+10
+
+Output
+
+Even
+
+Input
+
+5
+
+Output
+
+Odd
+
+
 import java.util.*;
 class Day5{
     public static void main(String[] args){
