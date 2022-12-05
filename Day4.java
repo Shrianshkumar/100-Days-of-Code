5fbc12c800edcf15d5d5271df0f56d01259a7d58
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-05 19:35:45 +0530

    Day4

---
+++ Day4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Day4{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter: ");
+        int num=sc.nextInt();
+        if(num>0){
+            System.out.println("Positive Number");
+        }else{
+            System.out.println("Negative Number");
+        }
+    }
+}
