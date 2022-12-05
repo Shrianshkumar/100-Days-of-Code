32173f4f58b4b047d9d4b9a1eab77b4c26bdf6c5
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-05 20:03:54 +0530

    Day6

--- Day6.java
+++ Day6.java
@@ -1,3 +1,24 @@
+Day 6 coding Statement:  Write a program to find the Quadrants in which coordinates lie
+
+Get the value of x and y coordinates as input from the user and check in which quadrant the point lies and print it.
+
+Input
+
+10 20
+
+Output
+
+This point lies in first quadrant.
+
+Input
+
+-10 20
+
+Output
+
+This point lies in second quadrant.
+
+
 import java.util.*;
 class Day6{
     public static void main(String[] args){
