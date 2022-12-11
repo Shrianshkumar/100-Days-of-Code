ce7796054c629c89f9553b0956e45b04a85cf9f4
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-11 15:56:53 +0530

    Add files via upload

---
+++ Day9.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Day9{
+    public static void main(String[] args){
+        int count;
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        String n = Integer.toString(num);
+        System.out.println(+n.length());
+    }
+}
