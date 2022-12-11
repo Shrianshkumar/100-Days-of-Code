8ed5ed7306d5935559db319585752df4f0f997e2
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-11 15:55:19 +0530

    Day7

--- Day7.java
+++ Day7.java
@@ -1,3 +1,20 @@
+Day 7 coding Statement:  Write a program to find Number of days in a given month of a given year
+
+Description
+
+Get the number of month and year as input from the user and check the number of days present in that month.
+
+Input
+
+Enter month : 2
+
+Enter year : 2000
+
+Output
+
+29
+
+
 import java.util.*;
 class Day7{
     public static void main(String[] args){
