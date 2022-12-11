718093e99fb13a8af1eefc041d47c7825c586cce
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-11 15:57:31 +0530

    Day9

--- Day9.java
+++ Day9.java
@@ -1,3 +1,26 @@
+Day 9 coding Statement : Write a program to find Number of digits in an integer
+
+Description
+
+Take an integer as the input from the user and then calculate the number of digits in the given input and print it as the output.
+
+Input
+
+3241
+
+Output
+
+4 I
+
+nput
+
+6
+
+Output
+
+1
+
+
 import java.util.*;
 public class Day9{
     public static void main(String[] args){
