698b3a4199f1185f5a812a8b3d10a30e5192c0c4
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-11 20:54:05 +0530

    Day10

--- Day10.java
+++ Day10.java
@@ -1,3 +1,18 @@
+Day 10 coding Statement:  Write a program to find Factorial of a number
+
+Description
+
+Get a number from user for which you need to fin the factorial, then calculate the factorial and give it as the output.
+
+Input
+
+4
+
+Output
+
+24
+
+
 import java.util.*;
 public class Day10{
     public static void main(String[] args){
