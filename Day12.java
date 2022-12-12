01b219efdbfeacab830591f06f26a3b94b62227f
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-12 18:17:52 +0530

    Day12

--- Day12.java
+++ Day12.java
@@ -1,3 +1,22 @@
+Day 12 coding Statement:  Write a program to find Sum of digits of a number
+
+Description
+
+Get a number from user and then find the sum of the digits in the given number.
+
+E.g. let the number = 341
+
+Sum of digits is 3+4+1= 8
+
+Input
+
+4521
+
+Output
+
+12
+
+
 import java.util.*;
 public class Day12{
     public static void main(String[] args){
