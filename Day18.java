fb13f981bb24f708dbabd2ac5e6d494478e2af0e
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-21 19:22:00 +0530

    Day18

--- Day18.java
+++ Day18.java
@@ -1,3 +1,24 @@
+Day 18 coding Statement : Write a program to Add two fractions
+
+Description
+
+Get the values for numerator and denominator of two fractions, then add that fractions. Consider the following format
+
+x3/y3 = (x1/y1) + (x2/y2)
+
+here x3 = (x1*y2) + (x2*y1) and y3 = (y1*y2)
+
+Input
+
+2  3
+
+4  3
+
+Output
+
+2/1 
+
+
 import java.util.*;
 public class Day18{
     public static void main(String[] args){
