110708c70a7351401dc374a23031415f6597f3fc
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2022-12-21 19:13:54 +0530

    Day17

--- Day17.java
+++ Day17.java
@@ -1,3 +1,18 @@
+Day 17 coding Statement : Write a program to find the Factors of a number
+
+Description
+
+Get an input from the user and find the factors of the number.
+
+Input
+
+4
+
+Output
+
+1,2,4  
+
+
 import java.util.*;
 public class Day17{
     public static void main(String[] args){
