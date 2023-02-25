a743dc0c528ba47beb3c8b45e8aa03e984a7c7ea
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 22:57:35 +0530

    Add files via upload

---
+++ Day27.java
@@ -0,0 +1,14 @@
+//Write a program to find the double of the given number without using arithmetic operator
+
+import java.util.*;
+public class Day27{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        int num1=num;
+        for(int i=1;i<=num;i++){
+            num1++;
+        }
+        System.out.println(num1);
+    }
+}
