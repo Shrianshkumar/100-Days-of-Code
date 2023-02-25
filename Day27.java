39e80f605e20ede60e9cde42ca87495ac875f1ad
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 22:58:59 +0530

    Day 27

--- Day27.java
+++ Day27.java
@@ -1,3 +1,19 @@
+Day 27 coding Statement : Write a program to find the double of the given number without using arithmetic operator
+
+Description
+
+For the given input number calculate the double of it without using arithmetic operator.
+
+Input
+
+4
+
+Output
+
+8
+
+
+
 //Write a program to find the double of the given number without using arithmetic operator
 
 import java.util.*;
