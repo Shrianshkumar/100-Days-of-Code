d855fdfb7515d320e18afa94e120f2549908aa17
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 22:54:57 +0530

    Day 25

--- Day25.java
+++ Day25.java
@@ -1,3 +1,20 @@
+Day 25 coding Statement : Write a program to find Area of a circle
+
+Description
+
+Get the value for radius from the user and calculate the area of the circle for the given radius.
+
+Area of circle = 3.14*radius*radius
+
+Input
+
+3
+
+Output
+
+28.26
+
+
 import java.util.*;
 public class Day25{
     public static void main(String[] args){
