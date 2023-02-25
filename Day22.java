05651a4210bc178e32911075381fc1a48f453e27
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 22:50:37 +0530

    Day 22

--- Day22.java
+++ Day22.java
@@ -1,3 +1,18 @@
+Day 22 coding Statement : Write a program to express a number as a sum of two prime numbers
+
+Description
+
+Get a number as input from the user and express that number as sum of two prime numbers.
+
+Input
+
+4
+
+Output
+
+4 can be expressed as sum of 2 and 2
+
+
 import java.util.*;
 public class Day22{
   public static void main(String[] args) {
