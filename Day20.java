4c2ce958cd63fc4ee9fa9a76ae58332223d34f89
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 22:46:20 +0530

    Day 20

---
+++ Day20.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Day20{
+    public static void main(String[] args){
+        int count=0;
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
+        if(num==2){
+            System.out.println(num + " is a prime number");
+        }else{
+            for(int i=1;i<=num;i++){
+                if(num%i==0){
+                    count++;
+                }
+        }
+        if(count==2){
+            System.out.println(num + " is a prime number");
+        }else{
+            System.out.println(num + " is not a prime number");
+        }
+        }
+    }
+}
