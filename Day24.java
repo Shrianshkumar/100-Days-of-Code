23db4850f81515dc4199b554ed2670d7131e984d
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 22:53:35 +0530

    Day 24

--- Day24.java
+++ Day24.java
@@ -1,3 +1,24 @@
+Day 24 coding Statement : Write a program to print Pyramid pattern using stars
+
+Description
+
+Get the number of lines as the input and print stars in that many lines or rows in a pyramid shape.
+
+Input
+
+4
+
+Output
+
+  *
+
+ ***
+
+*****
+
+*******
+
+
 
 import java.util.*;
 public class Day24{
