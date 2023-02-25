1ccded8012cc9902ebe1cb0d326d6993913195f6
Shriansh Kumar <52628349+Shrianshkumar@users.noreply.github.com>
2023-02-25 22:48:58 +0530

    Day 21

--- Day21.java
+++ Day21.java
@@ -1,3 +1,26 @@
+Day 21 coding Statement : Write a program to identify if the number is Palindrome or not
+
+Description
+
+Get a number as input from the user and check whether that number is a Palindrome or not.
+
+Input
+
+121
+
+Output
+
+Palindrome
+
+Input
+
+34
+
+Output
+
+Not a Palindrome
+
+
 import java.util.*;
 public class Day21{
     public static void main(String[] args){
